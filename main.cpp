71d17061b24178b4c1abf2bc3a7a94c869f678ec
Austin Childress <bioshock4554@gmail.com>
2016-04-22 16:00:46 -0500

    New name generation

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 const string coursenames[10] = {"Biology", "English", "History", "Sports", "Math", "Chemistry", "Computer Science", "Spanish", "Music", "Art"};
 const string coursenums[4] = {"I", "II", "III", "IV"};
-const string studentnames[5] = {"Bob", "Joe", "John", "Dhruvan", "Elias"};
+const string firstnames[40] = {"James", "John", "Robert", "Michael", "William", "David", "Richard", "Joseph", "Charles", "Thomas", "Christopher", "Daniel", "Matthew", "Austin", "Anthony", "Mark", "Paul", "Steven", "George", "Kenneth", "Mary", "Patricia", "Jennifer", "Elizabeth", "Linda", "Barbara", "Susan", "Margaret", "Jessica", "Sarah", "Dorothy", "Karen", "Nancy", "Betty", "Lisa", "Sandra", "Ashley", "Kimberly", "Donna", "Helen"};
 
-struct school {
-	string name;
-	int students; 
-};
+const string lastnames[40] = {"Smith", "Johnson", "Williams", "Brown", "Jones", "Miller", "Davis", "Garcia", "Rodriguez", "Childress", "Martinez", "Anderson", "Taylor", "Thomas", "Hernandez", "Moore", "Martin", "Jackson", "Thompson", "White", "Lopez", "Lee", "Gonzalez", "Harris", "Clark", "Lewis", "Robinson", "Walker", "Perez", "Hall", "Young", "Allen", "Sanchez", "Wright", "King", "Scott", "Green", "Baker", "Adams", "Nelson"};
+const string schoolfirst[5] = {"Austin", "Hogwarts", "Elkins", "Public High", "United School of"};
+const string schoolsecond[2] = {"High School", "Consolidated"};
+const int MAX_STUDENTS = 100;
 
 struct course {
 	string name;
@@ -27,6 +27,11 @@ struct student {
 	course schedule[7];
 };
 
+struct school {
+	string name;
+	student students[100]; 
+};
+
 
 int random(int l, int h);
 void gen_schedule(course *gschedule);
@@ -36,10 +41,11 @@ void gen_student(student &gstudent);
 
 int main() {
 	srand(time(NULL));
-	student bob = {"Bob", 0, 0};
-	gen_schedule(bob.schedule);
-	for(int i = 0; i < 7; i++) {
-		cout << '\n' << bob.schedule[i].name;
+	school hs;
+	gen_school(hs);
+	cout << '\n';
+	for(int i = 0; i < MAX_STUDENTS; i++) {
+		cout << '\n' << hs.students[i].name;
 	}
 	cout << '\n';
 	return 0;
@@ -61,11 +67,14 @@ void gen_schedule(course *gschedule) {
 }
 
 void gen_school(school &gschool) {
-	
+	gschool.name = schoolfirst[random(0, 5)] + ' ' + schoolsecond[random(0, 5)];
+	for(int i = 0; i < MAX_STUDENTS; i++) {
+		gen_student(gschool.students[i]);
+	}
 }
 
 void gen_student(student &gstudent) {
-	gstudent.name = studentnames[random(0, 5)] + ' ' + studentnames[random(0, 5)];
+	gstudent.name = firstnames[random(0, 40)] + ' ' + lastnames[random(0, 40)];
 	gstudent.pop = random(0, 100);
 	gstudent.intel = random(0, 100);
 	gen_schedule(gstudent.schedule);
